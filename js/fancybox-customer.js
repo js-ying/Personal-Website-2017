50c6c574c92246c5068d4357b373708593d56a4e
JhihSi Ying <JhihSi Ying>
2017-04-04 14:04:59 +0800

    delete some useless file

---
+++ js/fancybox-customer.js
@@ -0,0 +1,14 @@
+$(".fancybox").fancybox({
+	openEffect  : 'elastic',
+	closeEffect : 'elastic',
+	prevEffect	: 'none',
+	nextEffect	: 'none',
+	helpers	: {
+		title	: {
+			type: 'outside'
+		},
+		overlay: {
+	    	locked: false
+	    }
+	}
+});
